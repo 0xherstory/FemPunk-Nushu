dae9b19920414f9066d516382d931a1b219c8b0a
Herstory <142302094+0xherstory@users.noreply.github.com>
2025-10-27 02:21:31 +0800

    Update layout.tsx

--- frontend/UI tsx files/layout.tsx
+++ frontend/UI tsx files/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { montserrat } from '../lib/fonts'
+import { montserrat } from '@/lib/fonts';
 import '../styles/figma-homepage/globals.css'
 
 export const metadata: Metadata = {
